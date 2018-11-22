82d50b9cf54c25e16bc8ef409932e6883c1dc51b
mariowr2 <mariomendez1234@live.com.mx>
2018-11-21 20:03:38 -0600

    added separation of dependencies for webapp and split script itself

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+#dependencies for running the script
+
 sudo apt install poppler-utils
 pip install pdf2image
 pip install reportlab
 pip install numpy
+
+# dependencies for webapp
 pip install Flask
+pip install Werkzeug
+
+
 echo "Finished"

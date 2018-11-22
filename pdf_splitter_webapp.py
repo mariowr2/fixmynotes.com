3c102fc797507e8edcd91d5035bb1100a98871b8
mariowr2 <mariomendez1234@live.com.mx>
2018-11-21 20:04:09 -0600

    edge cases covered, files are now saved and then served with a custom url

--- pdf_splitter_webapp.py
+++ pdf_splitter_webapp.py
@@ -1,40 +1,79 @@
-from flask import Flask , render_template, request, flash, url_for, redirect   #flask basic templates   request to get the actual file
-from flask_uploads import UploadSet, configure_uploads, patch_request_class #upload set used to instantiate the uploads object
+from flask import Flask , render_template, request, flash, url_for, redirect, send_from_directory
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
 import os
 
-# flask documentation at https://pythonhosted.org/Flask-Uploads/#flaskext.uploads.UploadSet
+
+#======================================================
+#	APP CONFIGURATION
+#======================================================
 
 
 app = Flask(__name__) #create flask object
 app.secret_key = 'secret' #secret cookie key for flash!
 
 
-
-pdf = UploadSet('pdf', 'pdf') # create Upload Set object
-
-#configure upload sets object
-
-app.config['UPLOADED_PDF_DEST'] = os.getcwd()+"/uploaded_files" 
-#!!!!app.config['UPLOADED_PDF_URL'] = URL WHERE FILES WILL BE SERVED
-#app.config['UPLOADED_PDF_ALLOW'] = '' ALLOWED FILE EXTENSIONS
-patch_request_class(app, 16777216) #set maximum size to 16MB
-configure_uploads(app, pdf) #set configure values
+app.config['UPLOAD_FOLDER'] = os.getcwd()+"/uploaded_files" #save path
+app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['pdf']) # allowed file extensions
 
 
+#=======================================================
+# DEFS
+#=======================================================
 
+def print_debug_msg(msg):
+	print "************************ "+msg
+
+#check the filename is allowed
+def allowed_filename(filename):
+	return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+#========================================================
+#	APP ROUTES
+#========================================================
+
+
+#file uploading
 @app.route('/', methods=['GET', 'POST'])
 def upload_pdf():
-	if request.method == 'POST':
-		if 'pdf' in request.files:
-			pdf_file = request.files['pdf']
-			filename = pdf.save(request.files['pdf'])
-			flash("Notes for " +filename+" F I X E D")
-			return render_template('succesful.html')		
-		else:
-			flash("Failed to upload")
-			return redirect(url_for('unsuccesful'))
-	return render_template('upload.html') # if not a post request, show the html
+	try:
+		if request.method == 'POST':
+			if 'pdf' in request.files:
+				pdf_file = request.files['pdf']
+				if not pdf_file.filename == '':
+					if pdf_file and allowed_filename(pdf_file.filename):
+						filename = secure_filename(pdf_file.filename) # make sure the filename is not dangerous
+						pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))# save the file!				
+						return redirect(url_for('uploaded_file',filename=filename))	
+					else:
+						flash("Only PDF's allowed ;)")
+						return redirect(url_for('unsuccesful'))
+				else:
+					flash("No file seleceted")
+					return redirect(url_for('unsuccesful'))
+			else:
+				flash("Failed to upload")
+				return redirect(url_for('unsuccesful'))
+
+	# TODO , SERVE CUSTOM PAGE WHEN THIS ERROR IS RAISED, SERVER CURRENTLY TERMINATES CONNECTION
+	except RequestEntityTooLarge:	
+		flash("Maximum file size is 21 MB ;)")
+		return redirect(url_for('unsuccesful'))
+	except:
+		flash("Unkown error ocurred. CONTACT ADMIN")
+		return redirect(url_for('unsuccesful'))
+
+
+	return render_template('upload.html') # if not a post request, show the html for submitting the file
+
+
+
+#file serving
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 
 @app.route('/unsuccesful')
@@ -45,8 +84,7 @@ def unsuccesful():
 def succesful():
 	return render_template('succesful.html')
 
-def print_debug_msg(msg):
-	print "************************ "+msg
+
 
 if __name__ == "__main__":
 	app.run()

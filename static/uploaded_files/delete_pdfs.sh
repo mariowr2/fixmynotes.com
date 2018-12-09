d8e921f7aa50164492739acb5ad14b3a048261c3
Mario Mendez <mariowr2@users.noreply.github.com>
2018-12-09 17:26:27 -0600

    Update delete_pdfs.sh
    added absolute paths to delete uploaded file after it is processed

--- static/uploaded_files/delete_pdfs.sh
+++ static/uploaded_files/delete_pdfs.sh
@@ -3,6 +3,6 @@
 #delete the file uploaded to the server for fixing after processing it
 
 FILENAME=$1
-DIRECTORY="static/uploaded_files/" 
+DIRECTORY="/var/www/Fix/Fix/static/uploaded_files/" 
 DIRECTORY+=$FILENAME
 rm "$DIRECTORY"
